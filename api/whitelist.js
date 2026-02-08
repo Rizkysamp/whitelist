b0f51142b829ff92edec34d8647371b0d67f1dd4
Rizkysamp <90831343+Rizkysamp@users.noreply.github.com>
2026-02-08 20:41:58 +0700

    Update whitelist.js

--- api/whitelist.js
+++ api/whitelist.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
     const panelUrl = "https://mizonetwork.galaxyhost.biz.id";
     const serverId = "ff861198";
-    const apiKey = "ptlc_DDPzaLZAwKYGGhtD6ZNJeSfFA9u026h4ExUYYY08jyL"; // PASTIKAN COPY ULANG DARI PANEL
+    const apiKey = "ptlc_2LlVbQGKnQNGWcBpJH4je8MCdp8JOuQNNphQgBRFKpi"; // PASTIKAN COPY ULANG DARI PANEL
 
     // Bersihkan URL panel (hapus / di akhir jika ada)
     const cleanUrl = panelUrl.replace(/\/$/, "");
